33e4bf938613b87d8ec38ea96145bd1b4955e8e3
anu-a1 <anupamaachuri@gmail.com>
2017-07-27 09:29:45 -0400

    Update AllocationTable.jsx

--- src/scripts/containers/AllocationTable.jsx
+++ src/scripts/containers/AllocationTable.jsx
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
 //import {exceljs} from 'exceljs'
 
 import ExcelIcon from '../../icons/Excel.png';
-import {fetchFIGGENAllocations} from '../actions/index';
+import {getFIGGENAllocations,postFIGGENAllocations} from '../actions/actions';
 
 
 
@@ -16,66 +16,63 @@ class AllocationTable extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { allocations: [{}]};
+        this.state = {modalIsOpen: false};
+
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+
+        this.onPostFIGGENAllocations = this.onPostFIGGENAllocations.bind(this);
     }
 
+    onPostFIGGENAllocations(event){
+        event.preventDefault();
+        this.openModal();
+        this.props.postFIGGENAllocations();
+        this.closeModal();
+    };
 
+    openModal() {
+        this.setState({modalIsOpen: true});
+        console.log('inside open modal');
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
+        console.log('inside close modal');
+    }
 
     componentDidMount()
     {
-
-        // fetch('/figgen/GetCalculatedAllocations')
-        //     .then( (response) => {
-        //         return response.json() })
-        //     .then( (json) => {
-        //         console.log(json);
-        //         this.setState({allocations: json[0]});
-        //     });
+        this.props.getFIGGENAllocations();
     }
-
-    // figgenPA()
-    // {
-    //     fetch('/figgen/CalculateAllocations')
-    //         .then(response => response.json())
-    //         .then(json => {
-    //             console.log(json);
-    //
-    //             if (json.Success){
-    //
-    //             }
-    //         });
-    // }
-
-    onButtonClickExample = event => {
-        event.preventDefault();
-        this.props.fetchFIGGENAllocations();
-    };
 
     render() {
         const {FIGGEN} = this.props;
+        console.log(this.props);
         return (
 
             <div className="container-blue" >
 
                 <div>use this data: {JSON.stringify(FIGGEN)}</div>
 
-                {/*<Modal isOpen={this.state.modalIsOpen} className = 'alloc_modal' contentLabel="Modal">*/}
-                    {/*<h5>Calculating Allocations </h5>*/}
-                    {/*{this.state.closeButtonDisplay ? <button onClick={this.closeModal}>close</button> : null}*/}
-                {/*</Modal>*/}
+                <button onClick={this.openModal}>Open Modal</button>
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onRequestClose={this.closeModal}
+                    contentLabel="Allocations Modal"
+                >
+                    <h2>Calculating Allocations</h2>
+                    <button onClick={this.closeModal}>close</button>
+
+                </Modal>
 
                 <div>
                     <div className="float-left" >
                         <h5>FIGGEN</h5>
                         <table><tbody>
                             <tr>
-                                <td><Button
-                                    onClick={this.onButtonClickExample}
-                                    bsStyle="primary"
-                                    bsSize="large"
-                                >Prepare </Button> </td>
+                                <td><Button onClick = {this.onPostFIGGENAllocations} bsStyle="primary" bsSize="large">Prepare </Button> </td>
                                 <td width={20}></td>
-
                                 <td><CSVLink data = {[]}>{<img src={ExcelIcon} alt="logotype" />}</CSVLink></td>
                                 <td width={60}></td>
                                 <td><Button bsStyle = "primary" bsSize = "large" onClick = {this.figgenPA}>Calculate </Button> </td>
@@ -164,7 +161,8 @@ function mapStateToProps({FIGGEN}) {
 }
 
 const mapDispatchToProps = {
-    fetchFIGGENAllocations
+    postFIGGENAllocations,
+    getFIGGENAllocations
 };
 
 export default connect(mapStateToProps, mapDispatchToProps) (AllocationTable);
